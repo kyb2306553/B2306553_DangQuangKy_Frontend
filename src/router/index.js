85c5e72949fa862d0ebc94caeefdfbf563e2d873
kyb2306553 <kyb2306553@student.ctu.edu.vn>
2026-07-20 20:19:18 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,12 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
+
+    {
+        path: "/:pathMatch(.*)*",
+        name: "notFound",
+        component: () => import("@/views/NotFound.vue"),
+    },
 ];
 
 const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import router from '@/router';
+
+</script>
+
+<template>
+    <div class="page">
+        <p>
+            Oops, không thể tìm thấy trang. Trở về
+            <router-link to="/">trang chủ.</router-link>
+        </p>
+    </div>
+</template>
