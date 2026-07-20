f8d2e22dc41dc29aeecd03ac50da656bf7096f7e
kyb2306553 <kyb2306553@student.ctu.edu.vn>
2026-07-20 22:15:41 +0700

    Tao trang cap nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -35,6 +35,15 @@
             <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact = "activeContact" />
+        <router-link :to="{
+            name: 'contact.edit',
+            params: { id: activeContact._id },
+        }"
+        >
+        <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i>Hiệu chỉnh
+        </span>
+        </router-link>
     </div>
 </div>
 </div>
@@ -44,6 +53,7 @@
     import InputSearch from "@/components/InputSearch.vue";
     import ContactList from "@/components/ContactList.vue";
     import ContactService from "@/services/contact.service";
+import router from "@/router";
 
     export default{
         components: {
